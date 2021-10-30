fcb51ed944bcdb5c6b50d1764b47158bbe6fb6ce
wagiminator <wagiminator@web.de>
2021-10-30 19:08:46 +0200

    Editorial changes

--- software/TinyCandle.ino
+++ software/TinyCandle.ino
@@ -1,23 +1,19 @@
-// TinyCandle - ATtiny13A
+// ===================================================================================
+// Project:   TinyCandle - Tealight Candle Simulation based on ATtiny13A
+// Version:   v1.0
+// Year:      2020
+// Author:    Stefan Wagner
+// Github:    https://github.com/wagiminator
+// EasyEDA:   https://easyeda.com/wagiminator
+// License:   http://creativecommons.org/licenses/by-sa/3.0/
+// ===================================================================================
 //
+// Description:
+// ------------
 // Simple tealight candle simulation with four LEDs for ATtiny13A.
 //
-//                          +-\/-+
-//        --- A0 (D5) PB5  1|°   |8  Vcc
-//        --- A3 (D3) PB3  2|    |7  PB2 (D2) A1 --- Button
-// MOSFET --- A2 (D4) PB4  3|    |6  PB1 (D1) ------ LED3/4 PWM
-//                    GND  4|    |5  PB0 (D0) ------ LED1/2 PWM
-//                          +----+    
-//
-// Controller:  ATtiny13A
-// Core:        MicroCore (https://github.com/MCUdude/MicroCore)
-// Clockspeed:  1.2 MHz internal
-// BOD:         disabled (to save energy)
-// Timing:      Micros disabled
-// Leave the rest on default settings. Don't forget to "Burn bootloader"!
-// No Arduino core functions or libraries are used. Use the makefile if 
-// you want to compile without Arduino IDE.
-//
+// References:
+// -----------
 // The simulation code is based on the great work of Mark Sherman.
 // https://github.com/carangil/candle
 //
@@ -25,31 +21,53 @@
 // Galois linear feedback shift register is taken from Łukasz Podkalicki.
 // https://blog.podkalicki.com/attiny13-pseudo-random-numbers/
 //
-// 2020 - 2021 by Stefan Wagner 
-// Project Files (EasyEDA): https://easyeda.com/wagiminator
-// Project Files (Github):  https://github.com/wagiminator
-// License: http://creativecommons.org/licenses/by-sa/3.0/
+// Wiring:
+// -------
+//                           +-\/-+
+//        --- RST ADC0 PB5  1|°   |8  Vcc
+//        ------- ADC3 PB3  2|    |7  PB2 ADC1 -------- Button
+// MOSFET ------- ADC2 PB4  3|    |6  PB1 AIN1 OC0B --- LED3/4 PWM
+//                     GND  4|    |5  PB0 AIN0 OC0A --- LED1/2 PWM
+//                           +----+
+//
+// Compilation Settings:
+// ---------------------
+// Controller:  ATtiny13A
+// Core:        MicroCore (https://github.com/MCUdude/MicroCore)
+// Clockspeed:  1.2 MHz internal
+// BOD:         BOD disabled
+// Timing:      Micros disabled
+//
+// Leave the rest on default settings. Don't forget to "Burn bootloader"!
+// No Arduino core functions or libraries are used. Use the makefile if 
+// you want to compile without Arduino IDE.
+//
+// Fuse settings: -U lfuse:w:0x2a:m -U hfuse:w:0xff:m
 
+
+// ===================================================================================
+// Libraries and Definitions
+// ===================================================================================
 
 // Libraries
-#include <avr/io.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
+#include <avr/io.h>               // for GPIO
+#include <avr/sleep.h>            // for sleep modes
+#include <avr/interrupt.h>        // for interrupts
+#include <util/delay.h>           // for delays
 
 // Pin definitions
-#define LED0          PB0
-#define LED1          PB1
-#define BUTTON        PB2
-#define UNUSEDPIN     PB3
-#define MOSFET        PB4
+#define LED0          PB0         // pin connected to LED 1/2
+#define LED1          PB1         // pin connected to LED 3/4
+#define BUTTON        PB2         // pin connected to button
+#define UNUSEDPIN     PB3         // unused pin
+#define MOSFET        PB4         // pin connected to MOSFET
 
 // Less delay accuracy saves 16 bytes flash
 #define __DELAY_BACKWARD_COMPATIBLE__ 1
 
-// -----------------------------------------------------------------------------
+// ===================================================================================
 // Pseudo Random Number Generator (adapted from Łukasz Podkalicki)
-// -----------------------------------------------------------------------------
+// ===================================================================================
 
 // Start state (any nonzero value will work)
 uint16_t rn = 0xACE1;
@@ -60,9 +78,9 @@ uint16_t prng(uint16_t maxvalue) {
   return(rn % maxvalue);
 }
 
-// -----------------------------------------------------------------------------
+// ===================================================================================
 // Candle Simulation Implementation (adapted from Mark Sherman)
-// -----------------------------------------------------------------------------
+// ===================================================================================
 
 // Candle simulation parameters
 #define MINUNCALM     ( 5 * 256)
@@ -86,8 +104,8 @@ void updateCandle() {
   int16_t movy=0;
     
   // Random trigger brightness oscillation, if at least half uncalm
-  if (uncalm > (MAXUNCALM / 2)) {
-    if (prng(2000) < 5) uncalm = MAXUNCALM * 2;  //occasional 'bonus' wind
+  if(uncalm > (MAXUNCALM / 2)) {
+    if(prng(2000) < 5) uncalm = MAXUNCALM * 2;  //occasional 'bonus' wind
   }
    
   // Random poke, intensity determined by uncalm value (0 is perfectly calm)
@@ -95,10 +113,10 @@ void updateCandle() {
   movy = prng(uncalm >> 8) - (uncalm >> 9);
   
   // If reach most calm value, start moving towards uncalm
-  if (uncalm < MINUNCALM) uncalmdir =  UNCALMINC;
+  if(uncalm < MINUNCALM) uncalmdir =  UNCALMINC;
   
   // If reach most uncalm value, start going towards calm
-  if (uncalm > MAXUNCALM) uncalmdir = -UNCALMINC;
+  if(uncalm > MAXUNCALM) uncalmdir = -UNCALMINC;
   uncalm += uncalmdir;
 
   // Move center of flame by the current velocity
@@ -106,14 +124,14 @@ void updateCandle() {
   centery += movy + (yvel >> 2); 
   
   // Range limits
-  if (centerx < -MAXDEV) centerx = -MAXDEV;
-  if (centerx >  MAXDEV) centerx =  MAXDEV;
-  if (centery < -MAXDEV) centery = -MAXDEV; 
-  if (centery >  MAXDEV) centery =  MAXDEV;
+  if(centerx < -MAXDEV) centerx = -MAXDEV;
+  if(centerx >  MAXDEV) centerx =  MAXDEV;
+  if(centery < -MAXDEV) centery = -MAXDEV; 
+  if(centery >  MAXDEV) centery =  MAXDEV;
 
   // Counter
   cnt++;
-  if (! (cnt & 3)) {
+  if(!(cnt & 3)) {
     // Attenuate velocity 1/4 clicks 
     xvel = (xvel * 999) / 1000;
     yvel = (yvel * 999) / 1000;
@@ -129,9 +147,9 @@ void updateCandle() {
   OCR0B = 128 + centery;
 }
 
-// -----------------------------------------------------------------------------
+// ===================================================================================
 // Main Function
-// -----------------------------------------------------------------------------
+// ===================================================================================
 
 // Main function
 int main(void) {
@@ -160,21 +178,21 @@ int main(void) {
   // Main loop
   while(1) {
     updateCandle();                     // candle simulation
-    if (~PINB & (1<<BUTTON)) {          // if button is pressed
+    if(~PINB & (1<<BUTTON)) {           // if button is pressed
       DDRB  &= ~((1<<LED0) | (1<<LED1));// LED pins as input (PWM off)
       PORTB &= ~(1<<MOSFET);            // switch off MOSFET
       _delay_ms(10);                    // debounce button
-      while (~PINB & (1<<BUTTON));      // wait for button released
+      while(~PINB & (1<<BUTTON));       // wait for button released
       _delay_ms(10);                    // debounce button
       sleep_mode();                     // sleep until button pressed
       DDRB  |= (1<<LED0) | (1<<LED1);   // LED pins as output (PWM on)
       PORTB |= (1<<MOSFET);             // switch on MOSFET
       _delay_ms(10);                    // debounce button
-      while (~PINB & (1<<BUTTON));      // wait for button released
+      while(~PINB & (1<<BUTTON));       // wait for button released
     }  
   _delay_ms(CANDLEDELAY);               // delay
   }
 }
 
 // Pin change interrupt service routine
-EMPTY_INTERRUPT (PCINT0_vect);          // nothing to be done here, just wake up from sleep
+EMPTY_INTERRUPT(PCINT0_vect);           // nothing to be done here, just wake up from sleep
